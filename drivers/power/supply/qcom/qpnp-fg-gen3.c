6cd42954ea70e5f93ec450e72b9b0c15dd88ae69
Nathan Chancellor <natechancellor@gmail.com>
2020-03-27 10:24:39 +0530

    power: qpnp-fg-gen3: Silence an instance of -Wsizeof-array-div in clear_cycle_counter
    Clang warns:
    
    ../drivers/power/supply/qcom/qpnp-fg-gen3.c:2611:32: warning: expression
    does not compute the number of elements in this array; element type is
    'u16' (aka 'unsigned short'), not 'u8 *' (aka 'unsigned char *')
    [-Wsizeof-array-div]
                            sizeof(chip->cyc_ctr.count) / sizeof(u8 *),
                                   ~~~~~~~~~~~~~~~~~~~  ^
    ../drivers/power/supply/qcom/qpnp-fg-gen3.c:2611:32: note: place
    parentheses around the 'sizeof(u8 *)' expression to silence this warning
    1 warning generated.
    
    Odds are this is intentional given the casting so silence the warning.
    
    Change-Id: I3b886700bf9221a9f4f5968513f3b51a14749ab2
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Git-commit: 7f1a7b9f26d8fe0ef2c6be6656e84580eca0ad1a
    Git-Repo: https://github.com/travarilo/kernel_asus_sdm660/commit/
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -3309,7 +3309,7 @@ static void clear_cycle_counter(struct fg_chip *chip)
 	}
 	rc = fg_sram_write(chip, CYCLE_COUNT_WORD, CYCLE_COUNT_OFFSET,
 			(u8 *)&chip->cyc_ctr.count,
-			sizeof(chip->cyc_ctr.count) / sizeof(u8 *),
+			sizeof(chip->cyc_ctr.count) / (sizeof(u8 *)),
 			FG_IMA_DEFAULT);
 	if (rc < 0)
 		pr_err("failed to clear cycle counter rc=%d\n", rc);
@@ -3921,7 +3921,7 @@ static int fg_somc_write_back_sram_params(struct fg_chip *chip)
 	/* Rewrite the CYCLE_COUNT */
 	rc = fg_sram_write(chip, CYCLE_COUNT_WORD, CYCLE_COUNT_OFFSET,
 			(u8 *)&chip->cyc_ctr.count,
-			sizeof(chip->cyc_ctr.count) / sizeof(u8 *),
+			sizeof(chip->cyc_ctr.count) / (sizeof(u8 *)),
 			FG_IMA_DEFAULT);
 	if (rc < 0) {
 		pr_err("Error in rewriting cycle counter, rc=%d\n", rc);
